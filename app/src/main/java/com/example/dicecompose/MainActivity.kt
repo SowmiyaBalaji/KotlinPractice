f9f7cbb1d99f0cab9ba622d28bb10c35e22e6eaf
Sowmiya Balaji <sowmiyabalaji027@gmail.com>
2024-02-28 09:52:59 +0530

    added btn

--- app/src/main/java/com/example/dicecompose/MainActivity.kt
+++ app/src/main/java/com/example/dicecompose/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.example.dicecompose
 
+import android.media.Image
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dicecompose.ui.theme.DiceComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DiceRoll(name = "Roll")
                 }
             }
         }
@@ -30,17 +46,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DiceRoll(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(painter = painterResource(R.drawable.dice_1), contentDescription = "1" )
+        Spacer(modifier = Modifier.height(16.dp))
+        var myval by remember {
+            mutableStateOf(false)
+        }
+        Button(onClick = { myval = !myval }) {
+            Text(text = "$myval")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DicePreview() {
     DiceComposeTheme {
-        Greeting("Android")
+        DiceRoll(name = "Roll", modifier = Modifier
+            )
     }
 }
